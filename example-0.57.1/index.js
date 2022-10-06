f77b22a998f1c0030d6cac7262c291a1aa4233be
Christian Finck <christian@finck.at>
2022-10-06 15:09:03 +0200

    removed examples

--- example-0.57.1/index.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import SwipeoutExample from './SwipeoutExample';
-
-AppRegistry.registerComponent(appName, () => SwipeoutExample);
